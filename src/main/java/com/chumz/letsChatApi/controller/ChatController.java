b76e62b65572fbc0b56dd1e79333a1fb9ba1cd65
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-07-15 15:33:18 +0200

    Update ChatController.java

--- src/main/java/com/chumz/letsChatApi/controller/ChatController.java
+++ src/main/java/com/chumz/letsChatApi/controller/ChatController.java
@@ -1,12 +1,11 @@
 package com.chumz.letsChatApi.controller;
 
-import com.chumz.letsChatApi.entity.ChatMessage;
+import com.chumz.letsChatApi.entity.Chat;
 import com.chumz.letsChatApi.service.ChatService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/chats")
@@ -15,8 +14,28 @@ public class ChatController {
     @Autowired
     private ChatService chatService;
 
+    @GetMapping
+    public List<Chat> getAllChats() {
+        return chatService.getAllChats();
+    }
+
+    @GetMapping("/{id}")
+    public Chat getChatById(@PathVariable Long id) {
+        return chatService.getChatById(id);
+    }
+
     @PostMapping("/sendMessage")
-    public ChatMessage sendMessage(@RequestBody ChatMessage chatMessage) {
-        return chatService.sendMessage(chatMessage);
+    public Chat sendMessage(@RequestBody Chat chat) {
+        return chatService.sendMessage(chat);
+    }
+
+    @PutMapping("/{id}")
+    public Chat updateMessage(@PathVariable Long id, @RequestBody Chat updatedChat) {
+        return chatService.updateMessage(id, updatedChat);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteMessage(@PathVariable Long id) {
+        chatService.deleteMessage(id);
     }
 }
