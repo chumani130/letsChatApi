04066f51cc5802ddc8b247d908373387945f8cb0
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-07-15 13:47:39 +0200

    Create FirebaseInitialization.java

---
+++ src/main/java/com/chumz/letsChatApi/firebase/FirebaseInitialization.java
@@ -0,0 +1,30 @@
+package com.chumz.letsChatApi.firebase;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+@Configuration
+public class FirebaseInitialization {
+
+    @PostConstruct
+    public void initialization() {
+        try {
+            FileInputStream serviceAccount = new FileInputStream("./serviceAccountKey.json");
+
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
+
+            FirebaseApp.initializeApp(options);
+        } catch (IOException e) {
+            throw new RuntimeException("Error initializing Firebase", e);
+        }
+    }
+}
