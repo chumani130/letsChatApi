a2d4e8cb0bcdf8aea8833fd74cc24d16be277780
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-07-15 13:47:41 +0200

    Update LetsChatApiApplication.java

--- src/main/java/com/chumz/letsChatApi/LetsChatApiApplication.java
+++ src/main/java/com/chumz/letsChatApi/LetsChatApiApplication.java
@@ -1,9 +1,12 @@
 package com.chumz.letsChatApi;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 public class LetsChatApiApplication {
 
 	public static void main(String[] args) {
