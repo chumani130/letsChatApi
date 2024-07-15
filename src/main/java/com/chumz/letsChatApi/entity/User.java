d4e5d560e1d91f5f7e743756de281a932661dc76
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2024-07-15 13:47:36 +0200

    Create User.java

---
+++ src/main/java/com/chumz/letsChatApi/entity/User.java
@@ -0,0 +1,18 @@
+package com.chumz.letsChatApi.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    private String id;
+    private String username;
+    private String email;
+
+}
